9cc118a928b83cf0f30acebb7fd2d00cb3a4501f
Elizabeth <ekkjohnson@gmail.com>
2022-05-17 19:20:41 -0500

    webpack

--- client/Develop/client/src/js/database.js
+++ client/Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
 };
 
 
-// TODO: Add logic for a method that gets all the content from the database
+// logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readonly');
